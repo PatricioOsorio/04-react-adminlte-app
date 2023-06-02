037ecfa396958ac5ae7e5ac37193b99a14d5cbe3
patricioosorio <patriciomiguel_12@hotmail.com>
2023-06-02 10:55:36 -0600

    add login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Content from './components/Content';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Aside from './components/Aside';
+// import Content from './components/Content';
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Aside from './components/Aside';
+import Login from './components/Login';
 
 function App() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
-      <Aside />
+      <Login />
+      {/* <Header /> */}
+      {/* <Content /> */}
+      {/* <Footer /> */}
+      {/* <Aside /> */}
     </>
   );
 }
